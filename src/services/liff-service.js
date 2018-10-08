ba007b5ce831ccb0ddfd64c490b75771d74145dd
kaegjhs <keaza_cc@hotmail.com>
2018-10-08 08:27:41 +0700

    Update liff-service.js

--- src/services/liff-service.js
+++ src/services/liff-service.js
@@ -8,7 +8,7 @@
 const request = require('request');
 const apiToken = 'YSBwEd4ftfY9CUMyMqXzs9AbVNqbV5eWBX3uUwVvL74iD0ksGcOo5Cii1UcRo0IK2Rglp9Spqr4ZUB7t5QtGo0S+XwPiMXwl3jEHkfC4NAk4EhYuyDzESDjfU7IdFQW6EPSsOEzggvTQd2gK26K3sQdB04t89/1O/w1cDnyilFU=';
 const apiRoute = 'https://api.line.me/liff/v1/apps';
-const herokuRoute = 'https://nodejs-line-api.herokuapp.com/';
+const herokuRoute = 'https://classattendance.herokuapp.com/';
 const headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer ' + apiToken
